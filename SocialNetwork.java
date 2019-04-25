29d5346656077555d5af594339563b3ccebe892e
FabianCMI <45429131+FabianCMI@users.noreply.github.com>
2019-04-25 10:19:20 +0200

    inscription finie

--- SocialNetwork.java
+++ SocialNetwork.java
@@ -20,25 +20,23 @@ public class SocialNetwork {
 		String adresseReseau = "172.20.128.64";
 		String motDePasse, login, sql;
 		int action;
-		JOptionPane menuPrincipal = new JOptionPane();
+		JOptionPane menuPrincipal = new JOptionPane(), menuInscription = new JOptionPane(), menuConnexion = new JOptionPane();
 		
 		//Connexion à la base de donnée
 		//int connexion = BD.ouvrirConnexion(adresseReseau, nomBD, loginAdmin, loginAdmin);
 		
 		String[] choix = {"Inscription", "Connexion", "Recherche"};
 		int rang = menuPrincipal.showOptionDialog(null, "Choississez votre action ", "Menu Principal", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, choix, choix[2]);
-		action = rang+1;
+		action = rang;
 		
 		switch(action){
 			//Inscription au réseau social
 			case 0 : {
-				Ecran.afficherln("Veuillez saisir votre identifiant : ");
-				login = Clavier.saisirString();
-				Ecran.afficherln("Veuillez saisir votre mot de passe (10 caractères maximum)");
-				motDePasse = Clavier.saisirString();
+				login = menuInscription.showInputDialog(null, "Veuillez saisir votre identifiant : ", "Inscription", JOptionPane.QUESTION_MESSAGE);
+				motDePasse = menuInscription.showInputDialog(null, "Veuillez saisir votre mot de passe (10 caractères maximum)", "Inscription", JOptionPane.QUESTION_MESSAGE);
 				while(motDePasse.length() > 10){
-					Ecran.afficherln("Votre mot de passe doit faire moins de 10 caractères. Saississez un autre mot de passe : ");
-					motDePasse = Clavier.saisirString();
+					menuInscription.showMessageDialog(null, "Votre mot de passe doit faire moins de 10 caractères. Saississez un autre mot de passe :" , "ERREUR - Mot de passe trop long", JOptionPane.ERROR_MESSAGE);
+					motDePasse = menuInscription.showInputDialog(null, "Veuillez saisir votre mot de passe (10 caractères maximum)", "Inscription", JOptionPane.QUESTION_MESSAGE);
 				}
 				sql = "INSERT INTO utilisateur (utLogin, utPassword) VALUES (" + login + ", " + motDePasse + ")";	
 				// insertion des données dans la table utilisateur
@@ -48,13 +46,12 @@ public class SocialNetwork {
 					login = Clavier.saisirString();
 				}*/
 				
-				Ecran.afficherln("Félicitations pour votre inscription sur " + nomReseauSocial + ". Nous allons maintenant vous demandez des informations pour compléter votre profil ! ");
-				Ecran.afficherln("Pour commencer, quelle est votre jeu vidéo préféré ? ");
-				String jvPref = Clavier.saisirString();
+				menuInscription.showMessageDialog(null, "Félicitations pour votre inscription sur " + nomReseauSocial + ". Nous allons maintenant vous demandez des informations pour compléter votre profil ! " , "Renseignement complémentaire", JOptionPane.INFORMATION_MESSAGE);
+				String jvPref = menuInscription.showInputDialog(null, "Pour commencer, quelle est votre jeu vidéo préféré ? ", "Informations complémentaires", JOptionPane.QUESTION_MESSAGE);
 				sql = "INSERT INTO jeu (jeTitre, jeJoueur) VALUES (" + jvPref + login + ")";
 				// insertion des données dans la table jeu	
 				//inscription = BD.executerUpdate(connexion, sql);
-				Ecran.afficherln("Et voilà, votre profil est finalisé ! ");
+				menuInscription.showMessageDialog(null, "Et voilà, votre profil est finalisé ! Nous vous souhaitons une agréable utilisation de " + nomReseauSocial + " ! ", "Inscription finalisée", JOptionPane.INFORMATION_MESSAGE);
 			}
 			break;	
 			
