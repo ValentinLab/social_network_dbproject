41015ba6a7e2d39a1adb2229b814848fc7658825
FabianCMI <45429131+FabianCMI@users.noreply.github.com>
2019-05-06 09:36:07 +0200

    Update SocialNetwork.java

--- SocialNetwork.java
+++ SocialNetwork.java
@@ -22,56 +22,27 @@ public class SocialNetwork {
 			return;
 		}
 		
-		/*do{
-			login = menuInscription.showInputDialog(null, "Veuillez saisir votre identifiant : ", "Inscription", JOptionPane.QUESTION_MESSAGE);
-			if(login == null){
-				return;
-			}
-			if(login.length() == 0){
-				menuInscription.showMessageDialog(null, "Ce champ est obligatoire, veuillez recommencer la saisie : " , "ERREUR - Login obligatoire-", JOptionPane.ERROR_MESSAGE);
-			}
-			if(login.length() > 30){
-				menuInscription.showMessageDialog(null, "Votre login ne peut pas excéder 30 caractères, veuillez recommencer la saisie : " , "ERREUR - Login obligatoire-", JOptionPane.ERROR_MESSAGE);
-			}
-		} while(login.length() == 0 || login.length() > 30);
-		do{
-			motDePasse = menuInscription.showInputDialog(null, "Veuillez saisir votre mot de passe (10 caractères maximum)", "Inscription", JOptionPane.QUESTION_MESSAGE);
-			if(motDePasse == null){
-				return;
-			}
-			if(motDePasse.length() > 10){
-				menuInscription.showMessageDialog(null, "Votre mot de passe ne doit pas faire plus de 10 caractères. Saississez un autre mot de passe :" , "ERREUR - Mot de passe trop long", JOptionPane.ERROR_MESSAGE);
-			}
-			if(motDePasse.length() == 0){
-				menuInscription.showMessageDialog(null, "Ce champ est obligatoire, veuillez recommencer la saisie : " , "ERREUR - Mot de passe obligatoire ", JOptionPane.ERROR_MESSAGE);
-			}
-		}while(motDePasse.length() == 0 || motDePasse.length() > 10);*/
 		
 		sql = "INSERT INTO utilisateur (utLogin, utPassword) VALUES (" + login + ", " + motDePasse + ")";	
 		// insertion des données dans la table utilisateur
-		/*int inscription = BD.executerUpdate(connexion, sql);
-		while (inscription == -1){
+		int inscription = BD.executerUpdate(connexion, sql);
+		sql = '"SELECT utLogin FROM utilisateur WHERE utLogin = "' + 'login'
+		while (BD.executerSelect(connexion, sql)){
 			menuInscription.showMessageDialog(null, "Votre identifiant doit être déjà pris, veuillez saisir en un autre : ", "ERREUR -  Identifiant déjà utilisé", JOptionPane.ERROR_MESSAGE);
-			login = verificationsSaisie("login", 30, menuInscription);
-		}*/
+			login = saisieCorrecte("Inscription", "login", 30, menuInscription);
+			if(login == null){
+				return;
+			}
+		}
 		
 		menuInscription.showMessageDialog(null, "Félicitations pour votre inscription sur " + nomReseauSocial + ". Nous allons maintenant vous demander des informations pour compléter votre profil ! " , "Renseignement complémentaire", JOptionPane.INFORMATION_MESSAGE);
 		jvPref = saisieCorrecte("Info complémentaire", "jeu vidéo préféré", 50, menuInscription);
 		if(jvPref == null){
 			return;
 		}
-		/*do{
-			jvPref = menuInscription.showInputDialog(null, "Quel est votre jeu vidéo préféré ? ", "Informations complémentaires", JOptionPane.QUESTION_MESSAGE);
-			if(jvPref == null){
-				return;
-			}
-			if(jvPref.length() == 0){
-				menuInscription.showMessageDialog(null, "Ce champ est obligatoire, veuillez recommencer la saisie : " , "ERREUR - Renseignement obligatoire ", JOptionPane.ERROR_MESSAGE);
-			}
-		}while(jvPref.length() == 0);*/
 		sql = "INSERT INTO jeu (jeTitre, jeJoueur) VALUES (" + jvPref + ", " + login + ")";
 		// insertion des données dans la table jeu	
-		//inscription = BD.executerUpdate(connexion, sql);
+		inscription = BD.executerUpdate(connexion, sql);
 		menuInscription.showMessageDialog(null, "Et voilà, votre profil est finalisé ! Nous vous souhaitons une agréable utilisation de " + nomReseauSocial + " ! ", "Inscription finalisée", JOptionPane.INFORMATION_MESSAGE);
 	}
 	
@@ -87,7 +58,7 @@ public class SocialNetwork {
 			return;
 		}
 		//  tests des données avec celles présentes dans la base de donnée
-		/*int res = BD.executerSelect(connexion, "SELECT* FROM utilisateur");
+		int res = BD.executerSelect(connexion, "SELECT* FROM utilisateur");
 		while (BD.suivant(res)) {
 			if( BD.attributString(res,"utLogin").equals(login)){
 				if(BD.attributString(res,"utPassword").equals(motDePasse)){
@@ -97,7 +68,7 @@ public class SocialNetwork {
 			}
 		}
 		BD.fermerResultat(res);
-		*/
+		
 		String[] choixConnexion = {"Rechercher des joueurs", "Gérer mon compte",  "Retour"};
 		int action = menuConnexion.showOptionDialog(null, "Que voulez-vous faire ?", "Menu Connexion", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, choixConnexion, choixConnexion[2]);
 		switch(action){
@@ -120,6 +91,7 @@ public class SocialNetwork {
 	public static String saisieCorrecte( String typeMenu, String typeSaisie,  int nbChar, JOptionPane menu){
 		String valeur;
 		do{
+
 			valeur = menu.showInputDialog(null, "Veuillez saisir votre " + typeSaisie + " : ", typeMenu + " - " + typeSaisie, JOptionPane.QUESTION_MESSAGE);
 			if(valeur == null){
 				return(null);
@@ -158,7 +130,8 @@ public class SocialNetwork {
 		boolean arret = false;
 		
 		//Connexion à la base de donnée
-		//int connexion = BD.ouvrirConnexion(adresseReseau, nomBD, loginAdmin, loginAdmin);
+		
+		int connexion = BD.ouvrirConnexion(adresseReseau, nomBD, loginAdmin, loginAdmin);
 		
 		do{
 			String[] choix = {"Inscription", "Connexion",  "Quitter"};
